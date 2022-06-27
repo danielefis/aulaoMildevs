86fdedf54b4880adf50cd8c92bc6f5de8c6c22dc
Daniel <94140639+danielefis@users.noreply.github.com>
2022-06-27 19:18:05 -0300

    Update SALARIOxHORA.py
    adicionado input de dias trabalhado...

--- SALARIOxHORA.py
+++ SALARIOxHORA.py
@@ -5,6 +5,7 @@ Exercício aprendido em: https://youtu.be/47bf2ZLkvxgg
 """
 salario = float(input("Valor do seu salário? "))
 hora = float(input("Horas trabalhada por dia? "))
-horaX = hora * 30
+dias = int(input("Dias trabalhado no mês? "))
+horaX = hora * dias
 horaTrabalhada = salario / horaX
-print(f"Valor por hora R$: {horaTrabalhada:.2f}")
+print(f"Valor por hora R$: {horaTrabalhada:.2f} O seu salário total em {dias} dias é de R$ {salario:.2f}")
