6ecbde0dd543e798d72aaaec1b3d201a3e317ceb
Daniel <94140639+danielefis@users.noreply.github.com>
2022-06-28 18:26:40 -0300

    Update carteira.py

--- carteira.py
+++ carteira.py
@@ -3,11 +3,21 @@
 Exercício aprendido em: https://youtu.be/47bf2ZLkvxgg
 
 """
+#saldo declarado nas variaveis: saldo_daniel e saldo_joao
+saldo_daniel = 200
+saldo_joao = 230
 
-saldo = 200
-pagar = float(input("Valor a pagar: "))
-if saldo >= pagar:
-    novo_saldo = (saldo - pagar)
-    print("Seu saldo atual é R$: ", novo_saldo)
+logar = str(input("Nome de usuário: "))
+if logar == "daniel":
+    saldo_daniel_pagar = float(input("Valor a pagar: "))
+    if saldo_daniel >= saldo_daniel_pagar:
+        novo_saldo_daniel = (saldo_daniel - saldo_daniel_pagar)
+    print("Seu saldo atual é R$: ", novo_saldo_daniel)
+
+elif logar == "joao":
+    saldo_joao_pagar = float(input("Valor a pagar: "))
+    if saldo_joao >= saldo_joao_pagar:
+            novo_saldo_joao = (saldo_joao - saldo_joao_pagar)
+            print("Seu saldo atual é R$: ", novo_saldo_joao)
 else:
     print("Saldo insuficiente!")
